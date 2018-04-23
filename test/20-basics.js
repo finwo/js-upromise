f5eeb8c3de494b2d1004f8a12f24cfa3a9940109
Robin Bron <robin@finwo.nl>
2018-04-23 16:26:49 +0200

    Scrutinizer remark on test/20-basics.js

--- test/20-basics.js
+++ test/20-basics.js
@@ -7,13 +7,13 @@ var UPromise = require('../dist/core');
 it('resolves as promised', function() {
   return UPromise.resolve("woof")
                 .then(function(m) { expect(m).to.equal('woof'); })
-                .catch(function(m) { throw new Error('was not supposed to fail'); })
+                .catch(function() { throw new Error('was not supposed to fail'); })
     ;
 });
 
 it('rejects as promised', function() {
   return UPromise.reject("caw")
-                .then(function(m) { throw new Error('was not supposed to succeed'); })
+                .then(function() { throw new Error('was not supposed to succeed'); })
                 .catch(function(m) { expect(m).to.equal('caw'); })
     ;
 });
